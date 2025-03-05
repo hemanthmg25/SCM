cdd9965f16b2e7e865a37fafc55d8880b4c429ed
hemanthmg25 <hemanth.g@s.amity.edu>
2025-03-05 21:21:16 +0530

    new file

---
+++ temp_conversion.c
@@ -0,0 +1,11 @@
+//progarm to convert fahrenheit to celcius
+#include <stdio.h>
+int main()
+{
+ float a;
+ printf("Enter the Temp in Fahrenheit:");
+ scanf("%f",&a);
+ printf("The Temp in celcius is %f:",((a-32)*5)/9);
+ return 0;
+}
+
